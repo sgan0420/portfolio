e72da359dd20b8f6672769e5e341299cd5906167
sgan0420 <shijiegan.gs@gmail.com>
2025-12-18 10:31:30 +0800

    remove bukit jalil

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -85,8 +85,8 @@ const Contact = () => {
     {
       icon: HiLocationMarker,
       label: "Location",
-      value: "Bukit Jalil, Kuala Lumpur",
-      href: "https://maps.google.com/?q=Bukit+Jalil,+Kuala+Lumpur",
+      value: "Kuala Lumpur, Malaysia",
+      href: "https://maps.google.com/?q=Kuala+Lumpur,+Malaysia",
     },
   ];
 
